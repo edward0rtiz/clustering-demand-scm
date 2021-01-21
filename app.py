738a904acdd23fbe127d1560839ac9581b6f4041
edward0rtiz <931@holbertonschool.com>
2021-01-21 08:38:52 -0500

    [feature] add app.y

---
+++ app.py
@@ -0,0 +1,28 @@
+#######################################################
+# Main APP definition.
+#
+# Dash Bootstrap Components used for main theme and better
+# organization.
+#######################################################
+
+import dash
+import dash_bootstrap_components as dbc
+
+# from flask_caching import Cache
+
+app = dash.Dash(
+    __name__,
+    external_stylesheets=[dbc.themes.CYBORG],
+    title="kiwibot analizer",
+)
+
+"""
+cache = Cache(app.server, config={
+    'CACHE_TYPE': 'filesystem',
+    'CACHE_DIR': 'cache-directory'
+})
+"""
+server = app.server
+
+# We need this for function callbacks not present in the app.layout
+app.config.suppress_callback_exceptions = False
