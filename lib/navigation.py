0ea026e0c774a2f755b0fabf16a311483a696cf0
edward0rtiz <931@holbertonschool.com>
2021-01-23 09:43:27 -0500

    [update] clean code create tabs

--- lib/navigation.py
+++ lib/navigation.py
@@ -1,12 +1,14 @@
-import dash
 import os
+
+import dash
 import dash_bootstrap_components as dbc
 import dash_core_components as dcc
 import dash_html_components as html
 from app import app
 
+from lib import analytics, clustering
 
-KIWI_LOGO = "../static/logo.png"
+KIWI_LOGO = "../assets/logo.png"
 
 # hypelinks
 kiwibot = "https://www.kiwibot.com/"
@@ -17,9 +19,9 @@ SIDEBAR_STYLE = {
     "top": 0,
     "left": 0,
     "bottom": 0,
-    "width": "16rem",
+    "width": "16%",
     "padding": "2rem 1rem",
-    "background-color": "#000000",  # f8f9fa
+    "background-color": "#000000",
 }
 
 
@@ -27,25 +29,83 @@ SIDEBAR_STYLE = {
 def navigation_bar():
     sidebar = html.Div(
         [
-            html.Img(src=KIWI_LOGO, height="40px"),
+            dbc.Button(
+                [html.Img(src=KIWI_LOGO, height="40px")],
+                href=kiwibot,
+                color="None",
+                block=True,
+            ),
             html.Hr(),
-            html.Hr(),
-            html.P(
+            html.H4(
                 "Analytical dashboard for strategic decisions",
-            ), html.Hr(),
+                style={"font-size": "1.2rem", "height": "60px", "text-align": "center"},
+            ),
             dbc.Nav(
                 [
-                    dbc.NavLink("Descriptive Analytics",
-                                href="/analytics", active="exact"),
-                    dbc.NavLink("Clustering Analytics",
-                                href="/clustering", active="exact"),
-                    dbc.NavLink("About Us", href="/about", active="exact"),
+                    dbc.NavLink(
+                        "Descriptive Analytics",
+                        href="/analytics",
+                        active=True,
+                        style={"font-size": "1rem", "color": "#FFFFFF"},
+                    ),
+                    dbc.NavLink(
+                        "Clustering Analytics",
+                        href="/clustering",
+                        active=True,
+                        style={"font-size": "1rem", "color": "#FFFFFF"},
+                    ),
+                    dbc.NavLink(
+                        "About Us",
+                        href="/about",
+                        active=True,
+                        style={"font-size": "1rem", "color": "#FFFFFF"},
+                    ),
                 ],
                 vertical=True,
                 pills=True,
+                className="nav nav-link:hover",
             ),
-            html.Div()
+            html.Div(
+                [
+                    dbc.Row(
+                        dbc.Col(
+                            html.Div("Copyright © 2021", style={"font-size": "0.8rem"}),
+                            style={"right": "-3em", "bottom": "-40em"},
+                        )
+                    ),
+                ]
+            ),
         ],
         style=SIDEBAR_STYLE,
     )
     return sidebar
+
+
+def content_eda():
+    content_1 = dbc.Container(
+        [
+            dbc.Row(
+                [
+                    dbc.Col(html.Div(navigation_bar()), width=2),
+                    dbc.Col([html.Br(), html.Div(analytics.a_layout)], width=10),
+                ]
+            )
+        ],
+        fluid=True,
+    )
+    return content_1
+
+
+def content_cluster():
+    content_2 = dbc.Container(
+        [
+            dbc.Row(
+                [
+                    dbc.Col(html.Div(navigation_bar()), width=2),
+                    dbc.Col([html.Br(), html.Div(clustering.c_layout)], width=10),
+                ]
+            )
+        ],
+        fluid=True,
+    )
+    return content_2
