7cd7ede48e053b0789d680dcfc5ad20eee3dc99d
edward0rtiz <931@holbertonschool.com>
2021-01-23 09:42:35 -0500

    [update] clean code

--- lib/data_engine/s3_download_all.py
+++ lib/data_engine/s3_download_all.py
@@ -1,22 +1,25 @@
-import pandas as pd
-import os
-import boto3
 import logging
-from botocore.exceptions import ClientError
+import os
 from io import StringIO
 
-AWS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
-AWS_SECRET = os.environ.get('AWS_SECRET_ACCESS_KEY')
+import boto3
+import pandas as pd
+from botocore.exceptions import ClientError
+
+AWS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
+AWS_SECRET = os.environ.get("AWS_SECRET_ACCESS_KEY")
 
 # Information of origin bucket and directory to download files
 bucket = "kiwi-bot"
-s3_folder = 'databases_csv/'
-filepath = 'kiwi_bot\data'
+s3_folder = "databases_csv/"
+filepath = "/data"
 
-s3 = boto3.resource('s3',
-                    region_name="us-east-1",
-                    aws_access_key_id=AWS_KEY_ID,
-                    aws_secret_access_key=AWS_SECRET)
+s3 = boto3.resource(
+    "s3",
+    region_name="us-east-1",
+    aws_access_key_id=AWS_KEY_ID,
+    aws_secret_access_key=AWS_SECRET,
+)
 
 
 def download_s3_folder(bucket_name, s3_folder, local_dir=None):
@@ -30,14 +33,17 @@ def download_s3_folder(bucket_name, s3_folder, local_dir=None):
     bucket = s3.Bucket(bucket_name)
 
     for obj in bucket.objects.filter(Prefix=s3_folder):
-        target = obj.key if local_dir is None \
+        target = (
+            obj.key
+            if local_dir is None
             else os.path.join(local_dir, os.path.relpath(obj.key, s3_folder))
+        )
         if not os.path.exists(os.path.dirname(target)):
             os.makedirs(os.path.dirname(target))
-        if obj.key[-1] == '/':
+        if obj.key[-1] == "/":
             continue
         bucket.download_file(obj.key, target)
-        print(f'file succesfully downloaded: {target[14:]}')
+        print(f"file succesfully downloaded: {target[14:]}")
 
 
 download_s3_folder(bucket_name=bucket, s3_folder=s3_folder, local_dir=filepath)
